e0b15f1adbacba66ad88a8e327fb0accac0d6237
Simon Garcia <simon.garciar@gmail.com>
2018-02-18 23:50:49 +0100

    Initial commit

---
+++ lib/ex_binance.ex
@@ -0,0 +1,18 @@
+defmodule ExBinance do
+  @moduledoc """
+  Documentation for ExBinance.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBinance.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
